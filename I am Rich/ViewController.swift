899204c930cce2e71f7dc736a1b27d7805320f3e
F3858208@bb.com.br <gcomartins@gmail.com>
2025-01-06 16:27:38 -0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Guilherme Martins on 06/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
